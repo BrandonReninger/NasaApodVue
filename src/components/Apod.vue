6629f63477718244d32c987b46ad67954f97e90b
Brandon Reninger <brandonreninger@yahoo.com>
2020-04-14 14:52:24 -0600

    a lil closer

--- src/components/Apod.vue
+++ src/components/Apod.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="apod col-6"></div>
+  <div class="apod col-6">
+    <img :src="apodData.imgUrl" />
+  </div>
 </template>
 
 <script>
 export default {
   name: "apod",
+  props: [apodData],
   data() {
     return {};
   },
-  computed: {}
+  methods: {
+    getApod() {
+      return this.$store.state.apod;
+    }
+  }
 };
 </script>
+
+<style>
+.apod {
+  outline: 1px solid black;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 Vue.use(Vuex)
 
 let _api = axios.create({
-  baseURL: 'https://api.nasa.gov/planetary/apod?api_key=C59OroTVdv2B7wJWzvYbPZeISZYz6SmgDMATc3JR',
+  baseURL: 'https://api.nasa.gov/planetary/apod?',
   timeout: 10000
 })
 
@@ -13,7 +13,24 @@ export default new Vuex.Store({
   state: {
     apod: {}
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    setApod(state, apod) {
+      state.apod
+    }
+  },
+  actions: {
+    async getApod({
+      commit,
+      dispatch
+    }) {
+      try {
+        let res = await _api.get('api_key=C59OroTVdv2B7wJWzvYbPZeISZYz6SmgDMATc3JR')
+        console.log(res.data)
+        commit('setApod', res.data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  },
   modules: {}
 })
